b4b7661987791207a450fb919b10e8269113f3da
José R. Ferrer Paris <jr.ferrer.paris@gmail.com>
2020-04-18 17:56:28 +1000

    change token length to match new tokens

--- R/hs_create.R
+++ R/hs_create.R
@@ -96,8 +96,8 @@ hs_create_results <- function(hs_create_response) {
 
 # Input validation ----
 
-# Check locally if the token is a 37-character string
+# Check locally if the token is a 48-character string
 is_valid_token <- function(token) {
-  if(nchar(token) != 37)
-    stop(token, " should be a 37-character string. See https://hypothes.is/profile/developer to generate your own token.")
+  if(nchar(token) != 48)
+    stop(token, " should be a 48-character string. See https://hypothes.is/profile/developer to generate your own token.")
 }
